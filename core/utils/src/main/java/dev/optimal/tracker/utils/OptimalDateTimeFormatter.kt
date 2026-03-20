d0d31f5aa500aff73d34b5716657a41367dda7cd
Paulo Rodrigues <paulorfrc@gmail.com>
2026-03-20 01:55:19 -0300

    chore: add locale to date formatters

--- core/utils/src/main/java/dev/optimal/tracker/utils/OptimalDateTimeFormatter.kt
+++ core/utils/src/main/java/dev/optimal/tracker/utils/OptimalDateTimeFormatter.kt
@@ -14,9 +14,22 @@ object OptimalDateTimeFormatter {
         return String.format(Locale.US, "%dh %02dm", hours, minutes)
     }
 
-    fun formatDate(date: LocalDateTime, showCurrentYear: Boolean = false): String {
+    fun formatDate(
+        date: LocalDateTime,
+        showCurrentYear: Boolean = false,
+        locale: Locale = Locale.ENGLISH): String
+    {
         val isCurrentYear = date.year == LocalDate.now().year
         val pattern = if (!showCurrentYear && isCurrentYear) "MMMM d" else "MMMM d, yyyy"
-        return date.toLocalDate().format(DateTimeFormatter.ofPattern(pattern))
+        return date.toLocalDate().format(DateTimeFormatter.ofPattern(pattern, locale))
+    }
+
+    fun formatMonth(date: LocalDateTime,
+                    showCurrentYear: Boolean = false,
+                    locale: Locale = Locale.ENGLISH): String
+    {
+        val isCurrentYear = date.year == LocalDate.now().year
+        val pattern = if (!showCurrentYear && isCurrentYear) "MMMM" else "MMMM yyyy"
+        return date.toLocalDate().format(DateTimeFormatter.ofPattern(pattern, locale))
     }
 }
--- feature/home/src/main/java/dev/optimal/tracker/home/HomeScreen.kt
+++ feature/home/src/main/java/dev/optimal/tracker/home/HomeScreen.kt
@@ -3,16 +3,21 @@ package dev.optimal.tracker.home
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.optimal.tracker.designsystem.theme.OptimalTheme
 import dev.optimal.tracker.home.components.SessionHistoryCard
+import dev.optimal.tracker.model.workout.WorkoutSessionModel
+import dev.optimal.tracker.utils.OptimalDateTimeFormatter
 
 @Composable
 fun HomeScreenRoute(
@@ -31,16 +36,41 @@ fun HomeScreen(
     uiState: HomeState,
     onStartWorkoutClick: () -> Unit
 ) {
+    val sessions = uiState.sessionHistory
     LazyColumn(
         modifier = Modifier.padding(horizontal = 16.dp),
         contentPadding = PaddingValues(top = 16.dp, bottom = 100.dp)
     ) {
-        items(uiState.sessionHistory) { session ->
+        itemsIndexed(sessions) { index, session ->
+            MonthIndicator(
+                currentSession = session,
+                previousSession = sessions.getOrNull(index - 1)
+            )
             SessionHistoryCard(
                 session = session,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
         }
+    }
+}
+
+@Composable
+fun MonthIndicator(
+    currentSession: WorkoutSessionModel,
+    previousSession: WorkoutSessionModel?,
+) {
+    val isNewMonth = previousSession == null ||
+            currentSession.startDate.month != previousSession.startDate.month ||
+            currentSession.startDate.year != previousSession.startDate.year
+
+    if (isNewMonth) {
+        Text(
+            text = OptimalDateTimeFormatter
+                .formatMonth(currentSession.startDate)
+                .uppercase(),
+            style = MaterialTheme.typography.labelSmall.copy(letterSpacing = 0.1.em),
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
     }
 }
 
